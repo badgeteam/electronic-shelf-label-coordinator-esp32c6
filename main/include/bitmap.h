35eaf0e404cabcba00ad060969c8ab5cb88b6486
Hugo Trippaers <hugo@trippaers.nl>
2023-06-11 00:10:26 +0200

    Working prototype

---
+++ main/include/bitmap.h
@@ -0,0 +1,10 @@
+//  SPDX-License-Identifier: MIT
+
+#ifndef EPD_STATION_BITMAP_H
+#define EPD_STATION_BITMAP_H
+
+#include <stdint.h>
+
+uint32_t verify_bitmap(const uint8_t *bitmap);
+
+#endif  // EPD_STATION_BITMAP_H
---
+++ main/include/ieee802154.h
@@ -0,0 +1,19 @@
+//  SPDX-License-Identifier: MIT
+
+#ifndef EPD_STATION_IEEE802154_H
+#define EPD_STATION_IEEE802154_H
+
+#include "freertos/FreeRTOS.h"
+#include "802154_proto.h"
+
+typedef struct {
+    uint8_t mode; // ADDR_MODE_NONE || ADDR_MODE_SHORT || ADDR_MODE_LONG
+    union {
+        uint16_t short_address;
+        uint8_t long_address[8];
+    };
+} ieee802154_address_t ;
+
+uint8_t ieee802154_header(const uint16_t *src_pan, ieee802154_address_t *src, const uint16_t *dst_pan, ieee802154_address_t *dst, uint8_t *header, uint8_t header_length);
+
+#endif  // EPD_STATION_IEEE802154_H
